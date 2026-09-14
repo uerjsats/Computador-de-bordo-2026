e9500e97b4fe0eff3ab56a1613227c4009951fe8
Caio Fitipaldi <caiofitipaldi@gmail.com>
2026-09-14 16:09:52 -0300

    Add: Biblioteca Wifi | Finalizado

---
+++ sensores/networkManager/networkManager.h
@@ -0,0 +1,35 @@
+#ifndef NETWORK_MANAGER_H
+#define NETWORK_MANAGER_H
+
+#include <WiFi.h>
+#include <Arduino.h>
+
+#define MAX_IMG_SIZE 30000
+
+extern volatile bool imageReady;
+
+extern const char* ssid;
+extern const char* password;
+extern const char*serverIP;
+extern const uint16_t port;
+
+// --- Declarações Externas ---
+extern uint32_t currentTotalIndex;
+extern float currentMissionTime;
+extern size_t currentImgSize;
+extern bool firstConnectionEver;
+
+extern size_t currentImgSize;
+extern uint32_t currentTotalIndex;
+extern float currentMissionTime;
+
+void setupNetwork();
+void resetRadio();
+void monitorConnection();
+bool receiveImage();
+
+// Buffer com a última imagem recebida via WiFi (preenchido em receiveImage())
+uint8_t* getImageBuffer();
+uint32_t getImageSize();
+
+#endif
